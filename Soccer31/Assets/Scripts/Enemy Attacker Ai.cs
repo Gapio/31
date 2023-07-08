10b2f045e1f9add5c69ec132276d63e1418ab423
Gapio <goksen.pakkan.6953@student.uu.se>
2023-07-08 16:56:22 +0200

    Ai done

--- Soccer31/Assets/Scripts/Enemy Attacker Ai.cs
+++ Soccer31/Assets/Scripts/Enemy Attacker Ai.cs
@@ -4,31 +4,64 @@ using UnityEngine;
 
 public class EnemyAttackerAi : MonoBehaviour
 {
-    public Transform Football;
+    public Transform footBall;
+    public Transform footBallLeft;
+    public Transform footBallRight;
+    public Transform currenTarget;
 
     float speed = 6.9f;
 
     Vector3 lookDirection;
 
     const float EPSILON = 0.1f;
-        
+
+    Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void Update()
     {
-        lookDirection = (Football.position - transform.position).normalized;
+        lookDirection = (footBall.position - transform.position).normalized;
 
         //if other side of the pitch
-        if (Football.position.z < 0)
+        if (footBall.position.z < 3.1)
         {
-            if ((transform.position - Football.position).magnitude > EPSILON)
+           /* if ((transform.position - footBall.position).magnitude > EPSILON)
             {
                 transform.Translate(lookDirection * Time.deltaTime * speed);
+            }*/
+
+            if(footBall.position.x < 0)
+            {
+                currenTarget = footBallRight;
+                lookDirection = (footBallRight.position - transform.position).normalized;
+                if ((transform.position - footBallRight.position).magnitude > EPSILON)
+                {
+                    transform.Translate(lookDirection * Time.deltaTime * speed);
+                }
+                // transform.Translate(1.69f * Time.deltaTime,0f,0f);
+                //GetComponent<ConstantForce>().force = new Vector3(6f,0f,0f);
+            }
+            else if(footBall.position.x > 0)
+            {
+                currenTarget = footBallLeft;
+                lookDirection = (footBallLeft.position - transform.position).normalized;
+                if ((transform.position - footBallLeft.position).magnitude > EPSILON)
+                {
+                    transform.Translate(lookDirection * Time.deltaTime * speed);
+                }
+                //transform.Translate(-1.69f * Time.deltaTime, 0f, 0f);
+                // GetComponent<ConstantForce>().force = new Vector3(-6f, 0f, 0f);
             }
         }
-        else if (0 > Football.position.z || Football.position.z < 9.31f)
+        else if (3.1 > footBall.position.z || footBall.position.z < 9.31f)
         {
-            if ((transform.position - Football.position).magnitude > EPSILON)
+            if ((transform.position - footBall.position).magnitude > EPSILON)
             {
-                transform.Translate(lookDirection * Time.deltaTime * speed/1.69f);
+                transform.Translate(lookDirection * Time.deltaTime * speed/1.5f);
             }
         }
         else if (transform.position.z < 9.31f)
@@ -36,6 +69,4 @@ public class EnemyAttackerAi : MonoBehaviour
             transform.Translate(0, 0, speed/1.31f * Time.deltaTime);
         }
     }
-    //if (ball is on the players side)
-
 }
